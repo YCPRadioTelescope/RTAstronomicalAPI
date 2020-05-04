5fd4fedc0772e262c0641d808a7bb9a6760f8fc2
Marie <thatthinker@gmail.com>
2020-05-04 12:54:10 -0400

    Properly added certain files

---
+++ Controllers/MiddlemanConnectionController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+
+namespace RTAstronomicalAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MiddlemanConnectionController : ControllerBase
+    {
+        [HttpGet]
+        public String Get(String command)
+        {
+            String responseData = "";
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 3333;
+                TcpClient client = new TcpClient(AstroAPIKey.getIP(), port); // Hosted Middleman
+                // TcpClient client = new TcpClient("localhost", port); // Localhost testing
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(command);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", command);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+                responseData = "ArgumentNullException: " + e;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                responseData = "SocketException: " + e;
+            }
+
+            return responseData; 
+        }
+    }
+}
